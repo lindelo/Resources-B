3771cb466e819d4448c7ef84668f35f168c96d45
AxelInd <axeltind@gmail.com>
2015-03-26 08:14:57 +0200

    Script file to run mongo
    Paths must be changed, but core functionality allows much easier access
    to and running of content for Resources

---
+++ Tester.ps1
@@ -0,0 +1,21 @@
+﻿$nodeJsPath='C:\Nodejs\node.exe';
+$workingDirectory = 'C:\Users\Axel\Documents\NetBeansProjects\COS301_MinProject_First\public_html\';
+
+#$currentFile = 'StoreImageInMongo.js';
+#$currentFile = 'Server.js';
+$currentfile = 'MongooseTest.js';
+
+cd $workingDirectory;
+
+
+#.\node.exe Server.js;
+#.\node.exe uploads/StoreImageInMongo.js;
+#./(ii $nodeJsPath $currentFile)
+#./$nodeJsPath $currentFile;
+#./node.exe.lnk .\StoreImageInMongo.js;
+#cd C:\Nodejs;
+
+$file = (Resolve-Path $workingDirectory$currentFile);
+
+./node $file;
+
---
+++ activateDB.ps1
@@ -0,0 +1,6 @@
+﻿$mongoPath = 'C:\Program Files\MongoDB\Server\3.0\bin\'
+
+cd $mongoPath;
+
+#.\mongod.exe --dbpath "C:\Program Files\MongoDB\Server\3.0\bin"
+.\mongod.exe --dbpath "C:\MongoDB_3_0_Standard"
